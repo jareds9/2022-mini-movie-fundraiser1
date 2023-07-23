5eadc133df1585e6e6b7628e6ba3c180decf6aba
jareds9 <140124868+jareds9@users.noreply.github.com>
2023-07-24 10:35:43 +1200

    Add files via upload

---
+++ 01_yes_no_vi.py
@@ -0,0 +1,17 @@
+#function
+
+
+
+
+
+
+#main
+want_instructions = input("Do you want to read the instructions? ")
+
+if want_instructions == "yes":
+    print("instructions go here")
+elif want_instructions == "no":
+    pass
+else:
+    print("please answer yes/no")
+
